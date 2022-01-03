dad00ccc6d5fdcfa977627ad7bbe6aebb922df75
sebastian <juanchavez@iwin.im>
2022-01-03 01:32:06 -0500

    -master : implementación del botom que permite agegar tres comics ramdon

--- src/app/core/services/marvel.service.ts
+++ src/app/core/services/marvel.service.ts
@@ -6,13 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class MarvelService {
-  public url = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=ee87a5cc32b1b1a21cd53721d9cd4a32&hash=786f85f258daa7b7a76f3647cc57346d';
   constructor(
     public _http: HttpClient
   ) { }
 
-  getHeroes(): Observable<any> {
-    return this._http.get(this.url)
+  getEntity(entity): Observable<any> {
+    let url = `http://gateway.marvel.com/v1/public/${entity}?ts=1&apikey=ee87a5cc32b1b1a21cd53721d9cd4a32&hash=786f85f258daa7b7a76f3647cc57346d`;
+    return this._http.get(url)
   }
 
   getComic(comicUrl): Observable<any> {
--- src/app/modules/main-view/main-view.component.ts
+++ src/app/modules/main-view/main-view.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, Component, ComponentFactoryResolver, ComponentRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { MarvelService } from 'src/app/core/services/marvel.service';
 import { ViewMoreComponent } from './components/view-more/view-more.component';
-import { filter } from 'rxjs/operators';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { forkJoin, from } from 'rxjs';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -11,6 +13,7 @@ export class MainViewComponent implements OnInit, AfterViewInit {
 
   public heroes = [];
   public favComics = [];
+  public allComics = []
   public page: number;
   filterComic = '';
   public simpleViewMore: ComponentRef<ViewMoreComponent> = null;
@@ -23,14 +26,21 @@ export class MainViewComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.page = 1;
     this.favComics = JSON.parse(localStorage.getItem('comics'));
-    this._marvelService.getHeroes().subscribe(
+    if (!this.favComics) {
+      this.favComics = []
+    }
+    this._marvelService.getEntity('characters').subscribe(
       res => {
         let { data } = res;
         this.heroes = data.results.map(({ ...hero }) => ({
           items: hero.comics?.items,
           ...hero
         }));
+        this.heroes.forEach(element => {
+          this.allComics = this.allComics.concat(element.items)
+        });
       });
+
   }
 
   ngAfterViewInit(): void {
@@ -39,12 +49,16 @@ export class MainViewComponent implements OnInit, AfterViewInit {
     this.simpleViewMore.instance.closeDialog.pipe(
       filter((comic) => !!comic),
     ).subscribe(comic => {
-      const index = this.favComics.findIndex(a => a.id === comic.id);
-      if (index < 0) {
-        this.favComics.push(comic)
-        localStorage.setItem('comics', JSON.stringify(this.favComics))
-      }
+      this.setComics(comic);
     })
+  }
+
+  setComics(comic) {
+    const index = this.favComics.findIndex(a => a.id === comic.id);
+    if (index < 0) {
+      this.favComics.push(comic)
+      localStorage.setItem('comics', JSON.stringify(this.favComics))
+    }
   }
 
   public async openViewMore(comic) {
@@ -52,7 +66,21 @@ export class MainViewComponent implements OnInit, AfterViewInit {
       this.simpleViewMore.instance.toggleModal();
       this.simpleViewMore.instance.comic = await res.data.results[0];
     })
-
   }
+
+  setRamdonComics() {
+    let randomComics =
+      [...this.allComics].sort(() => Math.random() > 0.5 ? 1 : -1).slice(0, 3);
+    const obs = from(randomComics)
+    obs.pipe(
+      mergeMap(comic => forkJoin({
+        comic: (this._marvelService.getComic(comic.resourceURI))
+      })
+      )
+    ).subscribe(res =>
+      this.setComics(res.comic.data.results[0])
+    )
+  }
+
 
 }
